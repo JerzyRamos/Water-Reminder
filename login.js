6567e57f7385b069d91427b92f65490917f92a29
Siqi Gao <gsiqi@byu.edu>
2020-02-13 16:27:21 -0700

    logo and setting

--- login.js
+++ login.js
@@ -1,6 +1,7 @@
-function registerUser(){
-    window.open('../home.html')
-}
-function loginUser(){
-    window.open('../home.html')
-}
+// function registerUser(){
+//     window.open('/home.html','_self')
+// }
+// function loginUser(){
+//     console.log("got login function")
+//     window.open('/home.html', '_self')
+// }
